0601827f848336abd165c41af30ee0eb02d5f0f4
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-07 23:02:43 +0530

    pls command function added

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const Discord=require('discord.js')
 const bot=new Discord.Client()
 const PREFIX='!';
+const SUFFIX='pls';
 bot.login(process.env.token);
 bot.on('ready',()=>{
     console.log('This bot  is online');
 })
+let argr=message.content.substring(SUFFIX.length).split(" ");
 bot.on('message',message=>{
  let args=message.content.substring(PREFIX.length).split(" ");
  switch(args[0])
  {
-     case 'help':message.channel.sendMessage('How can we help you');
+     case 'help':message.channel.sendMessage('How can we help you??');
      break;
      case 'Hi':message.reply('Hello')
      break;
 
+ }
+ switch(argr[0])
+ {
+     case 'youtube':message.reply.sendMessage('https://www.youtube.com/')
+     break;
+     case 'google':message.reply.sendMessage('https://www.google.com/')
+     break;
  }
 })
