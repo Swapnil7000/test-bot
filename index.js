9147cef2aeb8d9cb92a50c0b6190d90fb676093d
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-11 14:47:01 +0530

    test

--- index.js
+++ index.js
@@ -10,13 +10,13 @@ bot.on('message',message=>{
  let args=message.content.substring(PREFIX.length).split(" ");
  switch(args[0])
  {
-     case 'help':message.reply('How can we help you')
+     case 'help':message.channel.sendMessage('How can we help you')
      if(message.content===jusyllabus)
      {
-         message.channel.sendMessage('See the syllabus here https://jecrcuniversity.edu.in/assets/pdf/B.Tech.%20Computer%20Secience.pdf')
+         message.reply('See the syllabus here https://jecrcuniversity.edu.in/assets/pdf/B.Tech.%20Computer%20Secience.pdf')
      }
      break;
-     case 'Hi':message.channel.sendMessage('Hello')
+     case 'Hi':message.reply('Hello')
      break;
 
  }
@@ -26,9 +26,9 @@ bot.on('message',message=>{
     let argr=message.content.substring(SUFIX.length).split(" ");
     switch(argr[0])
     {
-        case 'youtube':message.channel.sendMessage('https://www.youtube.com/')
+        case 'youtube':message.channel.sendMessage('https://www.youtube.com/');
         break;
-        case 'Hi':message.channel.sendMessage('Hello')
+        case 'Hi':message.reply('Hello')
         break;
    
     }
