1efa65275924dcd07abf70f9aaed8940c64d4dbc
Swapnil bhojwani <53923373+Swapnil7000@users.noreply.github.com>
2020-02-11 15:23:02 +0530

    Pls command finalized

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord=require('discord.js')
 const bot=new Discord.Client()
 const PREFIX='!';
-const SUFIX='pls';
+const SUFIX='pls ';
 bot.login(process.env.token);
 bot.on('ready',()=>{
     console.log('This bot  is online');
