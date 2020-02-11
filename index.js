ce8443f9a463000029d4340fab08b3a8f3fbc73d
swapnil7000 <swapnil.bhojwani@gmail.com>
2020-02-11 14:37:22 +0530

    fixed

--- index.js
+++ index.js
@@ -1,40 +1,35 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client()
-const PREFIX = '!';
-const SUFIX = 'pls';
+const Discord=require('discord.js')
+const bot=new Discord.Client()
+const PREFIX='!';
+const SUFIX='pls';
 bot.login(process.env.token);
-bot.on('ready', () => {
+bot.on('ready',()=>{
     console.log('This bot  is online');
 })
-bot.on('message', message => {
-    let args = message.content.substring(PREFIX.length).split(" ");
-    switch (args[0].toLowerCase()) {
-        case 'help':
-            message.channel.sendMessage('How can we help you?? try jusyllabus')
-            break;
-        case 'jusyllabus':
-            message.reply('See the syllabus here https://jecrcuniversity.edu.in/assets/pdf/B.Tech.%20Computer%20Secience.pdf')
-            break;
-        case 'Hi':
-            message.reply('Hello')
-            break;
-        case 'embed':
-            var embed = new.Discord.embed()
-                .setDescription("This is an awesome embed!!")
-            message.channel.sendMessage(embed)
-            break;
-    }
+bot.on('message',message=>{
+ let args=message.content.substring(PREFIX.length).split(" ");
+ switch(args[0])
+ {
+     case 'help':message.channel.sendMessage('How can we help you');
+     if(message.content===jusyllabus)
+     {
+         message.reply('See the syllabus here https://jecrcuniversity.edu.in/assets/pdf/B.Tech.%20Computer%20Secience.pdf');
+     }
+     break;
+     case 'Hi':message.reply('Hello')
+     break;
+
+ }
 })
 
-bot.on('message', message => {
-    let argr = message.content.substring(SUFIX.length).split(" ");
-    switch (argr[0].toLowerCase()) {
-        case 'youtube':
-            message.channel.sendMessage('https://www.youtube.com/');
-            break;
-        case 'Hi':
-            message.reply('Hello')
-            break;
-
+bot.on('message',message=>{
+    let argr=message.content.substring(SUFIX.length).split(" ");
+    switch(argr[0])
+    {
+        case 'youtube':message.channel.sendMessage('https://www.youtube.com/');
+        break;
+        case 'Hi':message.reply('Hello')
+        break;
+   
     }
-})
+   })
